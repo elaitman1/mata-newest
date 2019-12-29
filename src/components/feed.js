fdfce24890c792bd9c04d08d4db6f04ccd9b4105
Eric Laitman <elaitman1@gmail.com>
2019-12-29 15:23:45 -0800

    feed sorted

--- src/components/feed.js
+++ src/components/feed.js
@@ -5,7 +5,7 @@ export default class Feed extends Component {
   state = {
     // current cell stores index and the actual cell object - index will be used for selecting element by id
     currentCell: [0, this.props.cells[Object.keys(this.props.cells)[0]]],
-    firstCellSelection: true
+    firstCellSelection: true,
   };
 
   // when selecting a new cell, swap class names to change blue border styling as well as setting state.
@@ -40,26 +40,69 @@ export default class Feed extends Component {
     });
   };
 
-  render = () => {
-    return (
-      <div className="feed-container">
-        <header className="feed-cells-container">{this.renderCells()}</header>
-        <section className="feed-items-container">
+  render(){
+   // let sorted = Object.keys(this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices).sort(function (a, b) {
+   //   var x = a.toUpperCase(),
+   //       y = b.toUpperCase();
+   //   if (x > y) {
+   //       return 1;
+   //   }
+   //   if (x < y) {
+   //       return -1;
+   //   }
+   //   return 0;
+   // })
 
-          {Object.keys(this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices).map((machSpecs, idx) => {
 
-            // machSpecs = this.state.currentCell[1].devices[machSpecs];
-            machSpecs = this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices[machSpecs];
-            return (
-              <FeedItem
-                key={idx}
-                machSpecs={machSpecs}
-                toggleMachineSelection={this.props.toggleMachineSelection}
-              />
-            )
-          })}
-        </section>
-      </div>
-    );
-  };
+//     return (
+//       <div className="feed-container">
+//         <header className="feed-cells-container">{this.renderCells()}</header>
+//         <section className="feed-items-container">
+//           {Object.keys(this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices).map((machSpecs, idx) => {
+//             debugger
+//             // machSpecs = this.state.currentCell[1].devices[machSpecs];
+//             machSpecs = this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices[machSpecs];
+//             return (
+//               <FeedItem
+//                 key={idx}
+//                 machSpecs={machSpecs}
+//                 toggleMachineSelection={this.props.toggleMachineSelection}
+//               />
+//             )
+//           })}
+//         </section>
+//       </div>
+//     );
+//   };
+// }
+
+let array = []
+
+Object.keys(this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices).map((machSpecs, idx) => {
+
+  // machSpecs = this.state.currentCell[1].devices[machSpecs];
+   array.push(this.props.cells[Object.keys(this.props.cells)[this.state.currentCell[0]]].devices[machSpecs]);
+ })
+
+ array.sort((a, b) => (a.name > b.name) ? 1 : -1)
+ console.log(array)
+
+return (
+  <div className="feed-container">
+    <header className="feed-cells-container">{this.renderCells()}</header>
+    <section className="feed-items-container">
+      {array.map((item, idx) => {
+        // machSpecs = this.state.currentCell[1].devices[machSpecs];
+        return (
+          <FeedItem
+            key={idx}
+            machSpecs={item}
+            toggleMachineSelection={this.props.toggleMachineSelection}
+          />
+        )
+      })}
+    </section>
+  </div>
+);
+};
 }
